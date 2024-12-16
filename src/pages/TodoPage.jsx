bd74c0f347423b9c6e0b002c96775320dbe06d46
Abhishek Bansal <abhishekbansal2312@gmail.com>
2024-12-16 22:54:36 +0530

    fixed height of the container

--- src/pages/TodoPage.jsx
+++ src/pages/TodoPage.jsx
@@ -59,7 +59,7 @@ export default function TodoPage() {
   console.log(leftTodos);
 
   return (
-    <div className="bg-gray-100 p-6 max-h-screen">
+    <div className="bg-gray-100 p-6 ">
       <div className="max-w-4xl min-h-screen mx-auto bg-white p-6 pt-2 flex justify-between flex-col">
         <div>
           <TodoForm onAddTodo={handleAddTodo} />
